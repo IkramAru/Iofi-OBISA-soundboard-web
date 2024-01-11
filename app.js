fd694fe224e62eb076622ac14b2477a8d091ef34
IkramAru <ikramarullahafif0@gmail.com>
2024-01-11 17:51:57 +0800

    Another update

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const audioElement = document.getElementById("OBISA!!!");
 
+const button = document.getElementById("obisa-button");
+button.addEventListener("click", playAudio);
+
+// Button press counter code
+const countElement = document.getElementById("count");
+id = "count";
+let count = 0;
+
+if (typeof Storage !== "undefined") {
+  if (localStorage.clickcount) {
+    count = Number(localStorage.clickcount);
+    countElement.textContent = `You have OBISA'd ${count} times.`;
+  }
+}
+
+button.addEventListener("click", () => {
+  count++;
+  localStorage.clickcount = count;
+  countElement.textContent = `You have OBISA'd ${count} times.`;
+  playAudio();
+});
+
 function playAudio() {
   audioElement.currentTime = 0;
   audioElement.play();
 }
-
-const button = document.getElementById("obisa-button"); // Target the specific button using its ID
-button.addEventListener("click", playAudio);
