1962a39090e4f096d066e04f521f9099035623cc
IkramAru <ikramarullahafif0@gmail.com>
2024-01-14 01:03:34 +0800

    sprite swap

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const audioElement = document.getElementById("OBISA!!!");
 const button = document.getElementById("obisa-button");
 const countElement = document.getElementById("count");
+const mainImage = document.getElementById("yopi");
 
 let count = 0;
 
@@ -16,6 +17,21 @@ button.addEventListener("click", () => {
   localStorage.clickcount = count;
   countElement.textContent = `You have OBISA'd ${count} times.`;
   playAudio();
+
+  // Image swapping with jumping effect
+  if (mainImage.src.includes("picture-A")) {
+    mainImage.src = "/pic/yopi-chibi-2.png";
+    mainImage.style.transform = "translateY(-30px)";
+    setTimeout(() => {
+      mainImage.style.transform = "translateY(0)";
+    }, 200);
+  } else {
+    mainImage.src = "/pic/yopi-chibi.png";
+    mainImage.style.transform = "translateY(-30px)";
+    setTimeout(() => {
+      mainImage.style.transform = "translateY(0)";
+    }, 200);
+  }
 });
 
 function playAudio() {
