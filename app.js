d6db7a5c880867c450b1e0ac176e839a45bf3fb2
IkramAru <ikramarullahafif0@gmail.com>
2024-01-14 14:56:28 +0800

    applied the gif sprites

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const countElement = document.getElementById("count");
 const mainImage = document.getElementById("yopi");
 
 let count = 0;
+let originalImageSrc = mainImage.src;
 
 if (typeof Storage !== "undefined") {
   if (localStorage.clickcount) {
@@ -18,22 +19,16 @@ button.addEventListener("click", () => {
   countElement.textContent = `You have OBISA'd ${count} times.`;
   playAudio();
 
-  // Image swapping logic within the click event listener
-  if (mainImage.src.includes("/pic/yopi-chibi-2.png")) {
-    mainImage.src = "/pic/yopi-chibi.png";
+  if (mainImage.src.includes("/pic/yopi-chibi-2.gif")) {
+    mainImage.src = "/pic/yopi-chibi.gif";
   } else {
-    mainImage.src = "/pic/yopi-chibi-2.png";
+    mainImage.src = "/pic/yopi-chibi-2.gif";
   }
 
-  // Temporary image swap with transition and delay
-  const originalSrc = mainImage.src; // Store original image URL
-  mainImage.src = "/pic/yopi-chibi-2.png"; // Replace with alternative image
   mainImage.style.transition = "transform 0.3s ease-in-out";
   mainImage.style.transform = "translateY(-30px)";
-
   setTimeout(() => {
     mainImage.style.transform = "translateY(0)";
-    mainImage.src = originalSrc; // Revert to original image after delay
   }, 300);
 });
 
@@ -41,19 +36,3 @@ function playAudio() {
   audioElement.currentTime = 0;
   audioElement.play();
 }
-
-// Idle animation function
-function idleAnimation() {
-  mainImage.style.transition = "transform 1s ease-in-out";
-  mainImage.style.transform = "rotate(-5deg)";
-
-  setTimeout(() => {
-    mainImage.style.transform = "rotate(5deg)";
-  }, 500);
-
-  setTimeout(() => {
-    idleAnimation();
-  }, 1000);
-}
-
-idleAnimation(); // Start the idle animation
